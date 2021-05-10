9b0c4b29355c3357f4d5c022cce9bfd9ce31aa69
adrian310303 <83965376+adrian310303@users.noreply.github.com>
2021-05-10 21:09:51 +0700

    Add files via upload

---
+++ jatidiri.py
@@ -0,0 +1,44 @@
+nama = "kayla"
+format_str = f"nama= {nama}"
+
+print(format_str)
+
+data = "perempuan"
+format_str = f"kelamin= {data}"
+
+print(format_str)
+
+data2 = "smp130"
+format_str = f"lulusan= {data2}"
+
+print(format_str)
+
+data3 = "14"
+format_str = f"umur= {data3}"
+
+print(format_str)
+
+g = "2005 -12 -31"
+format_str = f"tanggal lahir= {g}"
+
+print(format_str)
+
+x = "indonesia"
+format_str = f"negara= {x}"
+
+print(format_str)
+
+a = "DKI JAKARTA"
+format_str = f"provinsi= {a}"
+
+print(format_str)
+
+b = "jln ks tubun 1 kota bambu selatan rt 04 rw 04 no 25 · Kota Bambu Selatan, DKI Jakarta, Kota Jakarta Barat, Palmerah Palmerah , Kota Jakarta Barat , DKI Jakarta"
+format_str = f"alamat lengkap= {b}"
+
+print(format_str)
+
+h = "+6285891169735"
+format_str = f"no telfon= {h}"
+
+print(format_str)
