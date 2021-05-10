a899f32929714a2795c3b73da94e90f7fa394068
adrian310303 <83965376+adrian310303@users.noreply.github.com>
2021-05-10 21:19:52 +0700

    Add files via upload

---
+++ adrian.py
@@ -0,0 +1,32 @@
+data = [1,2,3,17,16,19]
+
+subdata = data [5]
+subdata2 = data [-5]
+
+subdata3 = data [0:6]
+subdata4 = data [-5:]
+
+data2 = [100,200,500,800,300]
+
+
+data3 = data + data2
+
+data[4] = 87
+a = data [:]
+a[5] = 13
+#pusing gwtaiii
+
+data[2:4] = 31,31
+
+
+x = [data,data2]
+
+y = x[1] [2]
+
+#methods list
+data.append(30)
+
+panjang_list = len(data)
+
+print(data)
+print(panjang_list)
---
+++ ktp.py
@@ -0,0 +1,24 @@
+nama = "kayla"
+format_str = f"nama= {nama}"
+
+print(format_str)
+
+angka = 2005 
+format_str = f"tahun= {angka}"
+
+print(format_str)
+
+apa = 12
+format_str = f"bulan= {apa}"
+
+print(format_str)
+
+tanggal = 31
+format_str = f"tanggal= {tanggal}"
+
+print(format_str)
+
+pelajaran = 130
+format_str = f"lulusan= {pelajaran}"
+
+print(format_str)
